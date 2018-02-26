c214bad74309b8a3bf6c6f43cbd86084e62809d7
gmatere <gmatere@my.bridgeport.edu>
2018-02-25 20:36:15 -0500

    test
    first commit

---
+++ test.py
@@ -0,0 +1,30 @@
+from neo4j.v1 import GraphDatabase
+uri = "bolt://im-interview-test-8d5jfp.clearlinkdata.com:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "5T\z8kV#n!E/a{'7"))
+
+rid = set()
+with open("/home/osboxes/Desktop/mvc.data") as f:
+    next(f)
+    with driver.session() as session:
+        for line in f:
+            a = line.split('\t')
+            print a
+            result = session.run(
+                "MERGE (r:Rid {id:'" + a[1] + "' }) MERGE (c:Cid { id: '" + a[2] + "'})  CREATE UNIQUE  (r)-[:HAS]->(c);")
+            print a
+
+
+
+
+with open("/home/osboxes/Desktop/moc.data") as f:
+    next(f)
+    with driver.session() as session:
+        for line in f:
+            a = line.split('\t')
+            print a
+            result = session.run(
+                "MERGE (o:Oid {id:'" + a[1] + "' }) MERGE (c:Cid { id: '" + a[2] + "'})  CREATE UNIQUE  (o)-[:HAS]->(c);")
+            print a
+
+
+
